95285c828e710651482b576088e9d0e022040357
hilotter <hilotter3@gmail.com>
2020-06-20 19:40:19 +0900

    add puppeteer rendering script

---
+++ src/lambda/handlers/dynamic-rendering/index.js
@@ -0,0 +1,53 @@
+/* eslint-disable */
+require('source-map-support').install();
+
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+const isUrl = (url) => {
+  return !!url.match(/https?:\/\//);
+};
+
+let browser = null;
+
+exports.handler = async function (event) {
+  const query = event.queryStringParameters;
+  const url = query && query.url && decodeURI(query.url);
+  if (!url || !isUrl(url)) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'url required',
+      }),
+    };
+  }
+
+  console.log(url);
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+
+    const page = await browser.newPage();
+
+    // https://github.com/puppeteer/puppeteer/blob/main/src/common/DeviceDescriptors.ts
+    const iPhone = puppeteer.devices['iPhone X'];
+    await page.emulate(iPhone);
+
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    const html = await page.content();
+
+    await browser.close();
+
+    return {
+      statusCode: 200,
+      body: html,
+    };
+  } catch (error) {
+    console.error(error);
+    return { statusCode: 502, body: JSON.stringify({ message: error }) };
+  }
+};
